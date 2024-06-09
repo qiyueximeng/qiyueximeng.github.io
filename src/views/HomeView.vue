c818f89e049ed5cb156696ac2c063344e2917179
youya <qiyueximeng@gmail.com>
2024-06-09 17:05:57 +0800

    feat: modify presentation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,7 @@ const word = words[Math.floor(Math.random() * words.length)];
 const endYear = 2074;
 const curYear = (new Date()).getFullYear();
 const diffYear = endYear - curYear;
-const depositCount = 600000;
+const depositCount = 650000;
 const { consumeCount } = dichotomyCalcConsume({
   depositCount,
   depositRate: depositInterestRateAvg,
@@ -27,7 +27,12 @@ const consumeCountEveryMonth = (consumeCount / 12).toFixed(0);
   <main class="home">
     <h1 class="green">幽涯</h1>
     <h3>{{ word }}</h3>
-    <div>每月可支配金额为 {{ consumeCountEveryMonth }} 元</div>
+    <div class="presentation">
+      <p>真正意义上的自由不可实现，但我仍然希望达到尽可能的自由</p>
+      <p>在存款的数额能保障我正常生活到 80 岁时，开始想对自由的过</p>
+      <p>对能保障一个家庭正常开支的金额预算为每月 8000 元，需要将存款利息以及通胀都纳入计算</p>
+      <p>以我当前的存款，每月可支配金额为 {{ consumeCountEveryMonth }} 元，任重道远</p>
+    </div>
   </main>
 </template>
 
@@ -45,9 +50,17 @@ const consumeCountEveryMonth = (consumeCount / 12).toFixed(0);
   align-items: center;
 }
 
-.home > h1,
+.home > h1 {
+  margin-bottom: 2rem;
+}
+
 .home > h3 {
   margin-bottom: 1rem;
+}
+
+.presentation > p {
+  text-align: center;
+  margin-bottom: 0.5rem;
 }
 }
 </style>
